7b462f687204ee83b00673604bd211bdcd147fe8
odm275 <pomejia@gmail.com>
2022-04-25 00:12:49 -0500

    feat: ColorLink and mdx properly

--- content/components/ColorLink/ColorLink.tsx
+++ content/components/ColorLink/ColorLink.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Link } from "gatsby"
 import * as colorLinkStyles from "./colorLink.module.css"
 
-export const ColorLink = ({children, to}) => {
+enum Color {
+  name = "name",
+  work = "work",
+  thought = "thought",
+  cv = "cv",
+}
+
+interface Props {
+  children: ReactNode;
+  to: string;
+  color:Color;
+}
+
+export const ColorLink = ({children, to="/", color=Color.name}: Props) => {
+    const selectColor = {
+      name: colorLinkStyles.name,
+      work:colorLinkStyles.work,
+      thought: colorLinkStyles.thought,
+      cv: colorLinkStyles.cv
+    };
+
     return(
-        <Link to={to} className={`${colorLinkStyles.nameColor}`}>  
+        <Link to={to} className={`${colorLinkStyles.linkGeneralStyles} ${selectColor[color]}`}> 
           {children}
         </Link>
     );
--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Oscar Bautista`,
+    siteTitleAlt: `OscarBautista`,
+    siteHeadline: `Oscar Bautista`,
+    siteUrl: `https://www.whatoscarhasmade.com/`,
+    siteDescription: `Oscar Bautista Software Developer`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@spaceicediver`,
   },
   plugins: [
     {
